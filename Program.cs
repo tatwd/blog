6665920881e7c92b7a79d445fe6917bc08fc4d6c
tatwd <tatwdo@gmail.com>
2022-02-18 23:19:48 +0800

    refactor

--- Program.cs
+++ Program.cs
@@ -6,16 +6,34 @@ using RazorEngineCore;
 var cwd = Directory.GetCurrentDirectory();
 var distDir = $"{cwd}/dist";
 var postDir = $"{cwd}/posts";
-var styleDir = $"{cwd}/theme/styles";
-var postTemplateFilePath = $"{cwd}/theme/templates/post.cshtml";
-// var layoutTemplateFilePath = "./theme/templates/_layout.cshtml";
+var themeDir = $"{cwd}/theme";
+var themeTemplateDir = $"{themeDir}/templates";
+
+var themeStyleDir = $"{themeDir}/styles";
+var themePostTemplateFilePath = $"{themeTemplateDir}/post.cshtml";
+
+foreach (var path in Directory.GetFiles(themeStyleDir, "*", SearchOption.AllDirectories)) 
+{
+	if (path.StartsWith(themeTemplateDir))
+		continue;
+
+	var newPath = path.Replace(themeDir, distDir);
+	var fileDir = Path.GetDirectoryName(newPath);
+
+	if (string.IsNullOrEmpty(fileDir))
+		continue;
+
+	if (!Directory.Exists(fileDir))
+		Directory.CreateDirectory(fileDir);
+
+	File.Copy(path, newPath, overwrite: true);
+}
+
+
+
 
 var razorEngine = new RazorEngine();
-// IDictionary<string, string>  parts = new Dictionary<string, string>()
-// {
-// 	{"MyLayout", File.ReadAllText(layoutTemplateFilePath)},
-// };
-var templateContent = File.ReadAllText(postTemplateFilePath);
+var templateContent = File.ReadAllText(themePostTemplateFilePath);
 
 var pipline = new MarkdownPipelineBuilder()
 	// .UseAdvancedExtensions()
@@ -33,7 +51,7 @@ foreach (var path in filePaths)
 	var newPath = path.Replace(postDir, distDir + "/posts");
 
 	var newDir = Path.GetDirectoryName(newPath);
-	var filenameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
+	var fileNameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
 
 	if (string.IsNullOrEmpty(newDir))
 		continue;
@@ -47,38 +65,26 @@ foreach (var path in filePaths)
 		continue;
 	}
 
-	newPath = newPath.Replace(".md", ".html");
-
-
-	// Console.WriteLine(newPath);
 	var html = Markdown.ToHtml(mdText, pipline);
 
-	var template = razorEngine.Compile(templateContent);
-	// await template.SaveToFileAsync(Path.Combine(newPath, filenameWithoutExt + ".html"));
-	var result = template.Run(new
+	newPath = newPath.Replace(".md", ".html");
+	var fileName = Path.GetFileName(newPath);
+
+	var postViewModel = new
 	{
-		PostTitle = filenameWithoutExt,
+		PostTitle = fileNameWithoutExt,
 		PostContent = html,
-		PostUrl = newPath.Replace(distDir, "")
-	});
+		PostUrl = newPath.Replace(distDir, "").Replace(fileName, "")
+	};
+
+
+	var template = razorEngine.Compile(templateContent);
+	var result = template.Run(postViewModel);
+
 
 	using StreamWriter sw = File.CreateText(newPath);
-
 	sw.Write(result);
 }
 
 
-foreach (var path in Directory.GetFiles(styleDir, "*.css", SearchOption.AllDirectories)) 
-{
-	var newPath = path.Replace("./theme", distDir);
-	var fileDir = Path.GetDirectoryName(newPath);
-
-	if (string.IsNullOrEmpty(fileDir))
-		continue;
-
-	if (!Directory.Exists(fileDir))
-		Directory.CreateDirectory(fileDir);
-
-	File.Copy(path, newPath, overwrite: true);
-}
 
