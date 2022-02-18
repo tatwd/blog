8b9f62e6532075840c44048df093372c9f473d12
tatwd <tatwdo@gmail.com>
2022-02-18 23:27:24 +0800

    refactor

--- Program.cs
+++ Program.cs
@@ -4,15 +4,69 @@ using Markdig.Syntax;
 using RazorEngineCore;
 
 var cwd = Directory.GetCurrentDirectory();
+
 var distDir = $"{cwd}/dist";
 var postDir = $"{cwd}/posts";
 var themeDir = $"{cwd}/theme";
 var themeTemplateDir = $"{themeDir}/templates";
-
 var themeStyleDir = $"{themeDir}/styles";
 var themePostTemplateFilePath = $"{themeTemplateDir}/post.cshtml";
 
-foreach (var path in Directory.GetFiles(themeStyleDir, "*", SearchOption.AllDirectories)) 
+
+
+var razorEngine = new RazorEngine();
+var pipline = new MarkdownPipelineBuilder()
+	// .UseAdvancedExtensions()
+	.UseAutoIdentifiers()
+	.UseYamlFrontMatter()
+	// .UsePrism()
+	.Build();
+
+
+var themePostTemplateContent = File.ReadAllText(themePostTemplateFilePath);
+
+foreach (var path in Directory.GetFiles(postDir, "*", SearchOption.AllDirectories))
+{
+	var mdText = File.ReadAllText(path);
+
+	var newPath = path.Replace(postDir, distDir + "/posts");
+
+	var newDir = Path.GetDirectoryName(newPath);
+	var fileNameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
+
+	if (string.IsNullOrEmpty(newDir))
+		continue;
+
+	if (!Directory.Exists(newDir))
+		Directory.CreateDirectory(newDir);
+
+	// Copy others files to dist, just like images
+	if (!newPath.EndsWith(".md"))
+	{
+		File.Copy(path, newPath, true);
+		continue;
+	}
+
+	var html = Markdown.ToHtml(mdText, pipline);
+
+    var postUrl = Path.GetDirectoryName(newPath.Replace(distDir, ""));
+	var postViewModel = new
+	{
+		PostTitle = fileNameWithoutExt,
+		PostContent = html,
+		PostUrl = postUrl
+	};
+	var template = razorEngine.Compile(themePostTemplateContent);
+	var result = template.Run(postViewModel);
+
+
+	using StreamWriter sw = File.CreateText(newPath.Replace(".md", ".html"));
+	sw.Write(result);
+}
+
+
+// Copy static files
+foreach (var path in Directory.GetFiles(themeStyleDir, "*", SearchOption.AllDirectories))
 {
 	if (path.StartsWith(themeTemplateDir))
 		continue;
@@ -28,63 +82,3 @@ foreach (var path in Directory.GetFiles(themeStyleDir, "*", SearchOption.AllDire
 
 	File.Copy(path, newPath, overwrite: true);
 }
-
-
-
-
-var razorEngine = new RazorEngine();
-var templateContent = File.ReadAllText(themePostTemplateFilePath);
-
-var pipline = new MarkdownPipelineBuilder()
-	// .UseAdvancedExtensions()
-	.UseAutoIdentifiers()
-	.UseYamlFrontMatter()
-	// .UsePrism()
-	.Build();
-
-var filePaths = Directory.GetFiles(postDir, "*", SearchOption.AllDirectories);
-
-foreach (var path in filePaths) 
-{
-	var mdText = File.ReadAllText(path);
-
-	var newPath = path.Replace(postDir, distDir + "/posts");
-
-	var newDir = Path.GetDirectoryName(newPath);
-	var fileNameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
-
-	if (string.IsNullOrEmpty(newDir))
-		continue;
-
-	if (!Directory.Exists(newDir))
-		Directory.CreateDirectory(newDir);
-
-	if (!newPath.EndsWith(".md"))
-	{
-		File.Copy(path, newPath, true);
-		continue;
-	}
-
-	var html = Markdown.ToHtml(mdText, pipline);
-
-	newPath = newPath.Replace(".md", ".html");
-	var fileName = Path.GetFileName(newPath);
-
-	var postViewModel = new
-	{
-		PostTitle = fileNameWithoutExt,
-		PostContent = html,
-		PostUrl = newPath.Replace(distDir, "").Replace(fileName, "")
-	};
-
-
-	var template = razorEngine.Compile(templateContent);
-	var result = template.Run(postViewModel);
-
-
-	using StreamWriter sw = File.CreateText(newPath);
-	sw.Write(result);
-}
-
-
-
