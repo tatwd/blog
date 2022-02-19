13196c6dd805f85c00d43a0e57920db815cb53f0
tatwd <tatwdo@gmail.com>
2022-02-20 01:13:54 +0800

    feat: update

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 ﻿using Markdig;
+using Markdig.Parsers;
+using Markdig.Renderers;
 // using Markdig.Prism;
 using Markdig.Syntax;
 using RazorEngineCore;
 
-var cwd = Directory.GetCurrentDirectory();
+var blogTitle = "My Blog";
 
+var cwd = Directory.GetCurrentDirectory();
 var distDir = $"{cwd}/dist";
 var postDir = $"{cwd}/posts";
 var themeDir = $"{cwd}/theme";
-var themeTemplateDir = $"{themeDir}/templates";
 var themeStyleDir = $"{themeDir}/styles";
-var themePostTemplateFilePath = $"{themeTemplateDir}/post.cshtml";
-var themeHomeTemplateFilePath = $"{themeTemplateDir}/index.cshtml";
+var themeTemplateDir = $"{themeDir}/templates";
 
 
 if (Directory.Exists(distDir))
@@ -19,49 +20,71 @@ if (Directory.Exists(distDir))
 
 
 var razorEngine = new RazorEngine();
-var pipline = new MarkdownPipelineBuilder()
+var pipeline = new MarkdownPipelineBuilder()
     .UseAdvancedExtensions()
+    // .UseFigures()
     .UseYamlFrontMatter()
     // .UsePrism()
     .Build();
 
 
 // Generate post pages
-var posts = new List<PostViewModel>();
+var posts = new List<PostViewModel>(16);
+var themePostTemplateFilePath = $"{themeTemplateDir}/post.cshtml";
 var themePostTemplateContent = File.ReadAllText(themePostTemplateFilePath);
-foreach (var path in Directory.GetFiles(postDir, "*", SearchOption.AllDirectories))
+var postFiles = Directory.GetFiles(postDir, "*", SearchOption.AllDirectories);
+foreach (var path in postFiles.AsParallel())
 {
     var mdText = File.ReadAllText(path);
+    var newPath = path.Replace(postDir,  $"{distDir}/posts");
+    var postPageDir = Path.GetDirectoryName(newPath)!;
 
-    var newPath = path.Replace(postDir, distDir + "/posts");
 
-    var newDir = Path.GetDirectoryName(newPath);
-    var fileNameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
-
-    if (string.IsNullOrEmpty(newDir))
-        continue;
-
-    if (!Directory.Exists(newDir))
-        Directory.CreateDirectory(newDir);
+    if (!Directory.Exists(postPageDir))
+        Directory.CreateDirectory(postPageDir);
 
     // Copy others files to dist, just like images
     if (!newPath.EndsWith(".md"))
     {
         File.Copy(path, newPath, true);
+        Console.WriteLine("Generated: {0} (copyed)", newPath.Replace(distDir, ""));
         continue;
     }
 
-    var html = Markdown.ToHtml(mdText, pipline);
-
     var htmlFile = newPath.Replace(".md", ".html");
     var htmlFileName = Path.GetFileName(htmlFile);
-    var postRoute = Path.GetDirectoryName(htmlFile.Replace(distDir, ""));
+    var postRoute = Path.GetDirectoryName(htmlFile.Replace(distDir, ""))!;
+
+    using var writer = new StringWriter();
+    var renderer = new HtmlRenderer(writer)
+    {
+        LinkRewriter = (link) =>
+        {
+            if (link.StartsWith("./"))
+                return postRoute + link.Substring(1);
+            if (!link.StartsWith("/"))
+                return  postRoute + "/" + link;
+            return link;
+        }
+    };
+    pipeline.Setup(renderer);
+    var document = MarkdownParser.Parse(mdText, pipeline);
+    renderer.Render(document);
+    writer.Flush();
+    var html = writer.ToString();
+    // var html = Markdown.ToHtml(mdText, pipline);
+
+    var fileNameWithoutExt = Path.GetFileNameWithoutExtension(newPath);
+    var parentDir = Directory.GetParent(newPath)!;
+    var isPostPageDir = parentDir.FullName.EndsWith("/posts");
+    var postTitle = isPostPageDir ? fileNameWithoutExt : parentDir.Name;
+    var newPostRoute = isPostPageDir ? postRoute + "/" + htmlFileName : postRoute;
 
     var postViewModel = new PostViewModel
     {
-        PostTitle = fileNameWithoutExt,
+        PostTitle = postTitle,
         PostContent = html,
-        PostRoute = postRoute!
+        PostRoute = newPostRoute
     };
     posts.Add(postViewModel);
     var template = razorEngine.Compile(themePostTemplateContent);
@@ -69,22 +92,28 @@ foreach (var path in Directory.GetFiles(postDir, "*", SearchOption.AllDirectorie
 
     using StreamWriter swPost = File.CreateText(htmlFile);
     await swPost.WriteAsync(result);
+
+    Console.WriteLine("Generated: {0}/{1}", postRoute, htmlFileName);
 }
-Console.WriteLine("Generate all post pages ok!");
+
 
 // Generate index.html
-await RenderRazorPage(themeHomeTemplateFilePath, Path.Combine(distDir, "index.html"),
-    new { Posts = posts });
-Console.WriteLine("Generate home page ok!");
+await RenderRazorPageAsync($"{themeTemplateDir}/index.cshtml",
+    $"{distDir}/index.html", new
+    {
+        BlogTitle = blogTitle,
+        Posts = posts
+    });
+Console.WriteLine("Generated: /index.html");
 
 // Generate 404.html
-await RenderRazorPage(Path.Combine(themeTemplateDir, "404.cshtml"),
-    Path.Combine(distDir, "404.html"));
-Console.WriteLine("Generate 404 page ok!");
+await RenderRazorPageAsync($"{themeTemplateDir}/404.cshtml", $"{distDir}/404.html");
+Console.WriteLine("Generated: /404.html");
 
 
-// Copy other static files
-foreach (var path in Directory.GetFiles(themeDir, "*", SearchOption.AllDirectories))
+// Copy other files in theme directory
+var otherThemeFiles = Directory.GetFiles(themeDir, "*", SearchOption.AllDirectories);
+foreach (var path in otherThemeFiles.AsParallel())
 {
     if (path.StartsWith(themeTemplateDir))
         continue;
@@ -99,11 +128,11 @@ foreach (var path in Directory.GetFiles(themeDir, "*", SearchOption.AllDirectori
         Directory.CreateDirectory(fileDir);
 
     File.Copy(path, newPath, overwrite : true);
+    Console.WriteLine("Generated: {0} (copyed)", newPath.Replace(distDir, ""));
 }
-Console.WriteLine("Generate other static files ok!");
 
 
-async Task RenderRazorPage(string templatePath, string distPath, object? model = null)
+async Task RenderRazorPageAsync(string templatePath, string distPath, object? model = null)
 {
     if (razorEngine is null)
         return;
