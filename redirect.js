4cce33fe15310ea2cb730d575db9ee6f5da8aa8e
tatwd <tatwdo@gmail.com>
2023-10-14 16:29:33 +0800

    redirect: update

--- redirect.js
+++ redirect.js
@@ -1,16 +1,23 @@
 // Only used by Deno Deploy
 // my blog target domain
-const destWebsite = "https://tatwd-blog.vercel.app";
+const destDomain = "tatwd-blog.vercel.app";
 
 function handleRequest(request) {
-  const { pathname, host, searchParams } = new URL(request.url);
+  // const { pathname, host, searchParams } = new URL(request.url);
   // searchParams.set("__from", host);
+
+  const url = new URL(request.url);
+  url.host = destDomain;
+  const targetUrl = url.toString();
 
   const html = `<!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Refresh" content="0; URL=${destWebsite}${pathname}?${searchParams.toString()}" />
+  <meta http-equiv="Refresh" content="0; URL=${targetUrl}" />
 </head>
+<body>
+  Redirect to ${targetUrl} ...
+</body>
 </html>`;
   return new Response(html, {
     headers: {
