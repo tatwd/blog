5afdee373ea1cc4ba58137da759b45970df56e3a
tatwd <tatwdo@gmail.com>
2023-10-29 18:26:03 +0800

    docs: add comments & format

--- src/PostFrontMatter.cs
+++ src/PostFrontMatter.cs
@@ -9,26 +9,43 @@ public class PostFrontMatter
 
     [YamlMember(Alias = "create_time")]
     public DateTime CreateTime { get; set; }
+
     [YamlMember(Alias = "update_time")]
     public DateTime? UpdateTime { get; set; }
+
     [YamlMember(Alias = "tags")]
     public string[] Tags { get; set; } = Array.Empty<string>();
 
+    /// <summary>
+    /// 使用的模板名称  默认 posts/ 目录下默认为 post
+    /// </summary>
     [YamlMember(Alias = "template")]
     public string? TemplateName { get; set; }
 
+    /// <summary>
+    /// 是否是草稿 生产环境下不会编译输出草稿文件
+    /// </summary>
     [YamlMember(Alias = "draft")]
     public bool Draft { get; set; }
 
+    /// <summary>
+    /// 阅读时长
+    /// </summary>
     [YamlMember(Alias = "duration")]
     public string? Duration { get; set; }
 
+    /// <summary>
+    /// 文本内容摘要
+    /// </summary>
     [YamlMember(Alias = "description")]
     public string? Description { get; set; }
 
     [YamlMember(Alias = "lang")]
     public string Lang { get; set; } = "zh-CN";
 
+    /// <summary>
+    /// 是否启用文章目录功能
+    /// </summary>
     [YamlMember(Alias = "toc_enabled")]
     public bool EnabledToc { get; set; }
 
